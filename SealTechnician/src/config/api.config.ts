e748e9200d93f28825e70a2562d66576e2f11d6f
dabassawin <dabassawin12@gmail.com>
2026-02-20 16:26:15 +0700

    feat: add initial project dependencies and API configuration.

--- SealTechnician/src/config/api.config.ts
+++ SealTechnician/src/config/api.config.ts
@@ -5,21 +5,15 @@ import Constants from 'expo-constants';
 
 // Function to get the correct base URL based on environment
 const getBaseUrl = () => {
-  // If running on a physical device via Expo Go
-  if (Constants.expoConfig?.hostUri) {
-    const host = Constants.expoConfig.hostUri.split(':')[0];
-    return `http://${host}:3000/api`;
-  }
-
-  // For web or emulators
-  return 'http://localhost:3000/api';
+  // Always use the local network IP for Expo Go on physical device testing
+  return 'http://192.168.1.38:3000/api';
 };
 
 const BASE_URL = getBaseUrl();
 
 export const API_CONFIG = {
-  // Dynamic IP address
-  SERVER_IP: BASE_URL.split('//')[1].split(':')[0],
+  // IP address of your machine on the local network
+  SERVER_IP: '192.168.1.38',
   SERVER_PORT: '3000',
 
   // API endpoints
